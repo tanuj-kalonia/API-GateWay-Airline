6ab3e781a51c449985f23e21442b96d0e15b7bb6
Tanuj Kalonia <tanujkalonia2002@gmail.com>
2023-06-04 11:38:30 +0530

    Added Rate Limiter in the IP address of the user

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+
+const rateLimit = require('express-rate-limit')
 const { ServerConfig, LoggerConfig } = require("./config");
 
 const apiRoutes = require('./routes')
 const app = express();
 
+const limiter = rateLimit({
+    windowMs: 2 * 60 * 1000,
+    max: 3
+})
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(limiter);
 
 app.use('/api', apiRoutes);
 
