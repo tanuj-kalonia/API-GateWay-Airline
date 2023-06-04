7f803b5dfcfd97f55463e057779b6445ebf261b3
Tanuj Kalonia <tanujkalonia2002@gmail.com>
2023-06-04 13:58:03 +0530

    Added Backward Proxy in the API Gateway

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -6,5 +6,7 @@ module.exports = {
     PORT: process.env.PORT,
     SALT_ROUNDS: process.env.SALT_ROUNDS,
     JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRE: process.env.JWT_EXPIRE
+    JWT_EXPIRE: process.env.JWT_EXPIRE,
+    BOOKING_SERVICE: process.env.BOOKING_SERVICE,
+    FLIGHT_SERVICE: process.env.FLIGHT_SERVICE
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,35 @@
 const express = require('express');
-
+const { createProxyMiddleware } = require('http-proxy-middleware')
 const rateLimit = require('express-rate-limit')
 const { ServerConfig, LoggerConfig } = require("./config");
 
 const apiRoutes = require('./routes')
 const app = express();
 
-const limiter = rateLimit({
+const limiter = rateLimit({ // rate limiter -> avoids request attack on the services
     windowMs: 2 * 60 * 1000,
-    max: 3
+    max: 10
 })
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(limiter);
+
+/**
+ * Backward proxy for diffrent services
+ * All the Requests will be routed to corrosponding requests
+ */
+app.use('/flightsService', createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE,
+    changeOrigin: true,
+    pathRewrite: { '/flightsService': '/' }
+}))
+app.use('/bookingService', createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+    pathRewrite: { '/bookingService': '/' }
+}))
 
 app.use('/api', apiRoutes);
 
